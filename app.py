fa09c9a72a2a02a565c7240e5c282050ce2f70bb
Portia210 <yybarhen@gmail.com>
2025-03-15 23:51:36 +0200

    imporve dbs and website

--- app.py
+++ app.py
@@ -1,53 +1,13 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
-import sqlite3
-
-
-class Artist:
-    def __init__(self, artist_id, gr_name, en_name, number_of_songs, songs):
-        self.artist_id = artist_id
-        self.gr_name = gr_name
-        self.en_name = en_name
-        self.number_of_songs = number_of_songs
-        self.songs = songs
-
-    def __str__(self):
-        return f"{self.gr_name} {self.en_name} {self.number_of_songs}"
-    
-class Song:
-    def __init__(self, artist_id, gr_name, en_name, video_id, demo_id):
-        self.gr_name = gr_name
-        self.en_name = en_name
-        self.video_id = video_id
-        self.demo_id = demo_id
-        self.artist_id = artist_id
-
-    def __str__(self):
-        return f"{self.gr_name} {self.en_name} {self.video_id} {self.demo_id} {self.artist_id}"
-    
-
-def import_db():
-    conn = sqlite3.connect('instance/music.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM artists")
-    db_artists = cursor.fetchall()
-    cursor.execute("SELECT * FROM songs")
-    db_songs = cursor.fetchall()
-    conn.close()
-
-    artists = []
-    for artist in db_artists:
-        # song columns are: id	artist_id	original_name	en_name	gr_name	video_id	demo_id	more_videos
-        # convert demo_id and video_id to youtube links
-        artist_songs = [Song(song[1], song[2], song[3], song[5], song[6]) for song in db_songs if song[1] == artist[0]]
-        artist = Artist(artist[0], artist[2], artist[1], artist[3], artist_songs)
-        artists.append(artist)
-    
-    return artists
+from classes import import_db
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 Bootstrap5(app)
 
 
@@ -59,10 +19,10 @@ def home():
     return render_template("index.html", all_artists= all_artists)
 
 
-@app.route("/artist/<artist_id>")
-def artist_songs(artist_id):
+@app.route("/artist/<artist_name>")
+def artist_songs(artist_name):
     all_artists = import_db()
-    artist = [artist for artist in all_artists if artist.artist_id == int(artist_id)][0]
+    artist = [artist for artist in all_artists if artist.en_name == artist_name][0]
     print(artist)
     return render_template("artist_songs.html", artist=artist)
 
---
+++ classes.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+class Artist:
+    def __init__(self, en_name, gr_name, number_of_songs, songs):
+        self.gr_name = gr_name
+        self.en_name = en_name
+        self.number_of_songs = number_of_songs
+        self.songs = songs
+
+    def __str__(self):
+        return f"{self.gr_name} {self.en_name} {self.number_of_songs} {self.songs}"
+class Song:
+    def __init__(self, artist_name, en_name, gr_name, video_id, demo_id):
+        self.artist_name = artist_name
+        self.gr_name = gr_name
+        self.en_name = en_name
+        self.video_id = video_id
+        self.demo_id = demo_id
+
+    def __str__(self):
+        return f"{self.artist_name} {self.gr_name} {self.en_name} {self.video_id} {self.demo_id}"
+    
+def import_db():
+    conn = sqlite3.connect('instance/music.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM artists")
+    db_artists = cursor.fetchall()
+    cursor.execute("SELECT * FROM songs")
+    db_songs = cursor.fetchall()
+    conn.close()
+
+    artists = []
+    for artist in db_artists:
+        # artist columns are: artist_id	en_name	gr_name	number_of_songs	artist_img
+        # song columns are: song_id	artist_name	en_name	gr_name	video_id	demo_id
+        artist_songs = [Song(song[1], song[2], song[3], song[4], song[5]) for song in db_songs if song[1] == artist[1]]
+        artist = Artist(artist[1], artist[2], artist[3], artist_songs)
+        artists.append(artist)
+    return artists
