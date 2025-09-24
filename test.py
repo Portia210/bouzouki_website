c131019ba6f2942c9f2026bc109c74348ee21a99
Portia210 <yybarhen@gmail.com>
2025-09-24 15:42:33 +0300

    fixed encoding in json file

--- test.py
+++ test.py
@@ -1,27 +1,16 @@
 import json
 
-# artist dict(greek) > songs dict > song  dict
+file_path = "jsons/collections.json"
 
-def search_song(text):
-    with open("crawling_scripts/youtube_songs.json", "r") as file:
-        imported_json = json.load(file)
+with open(file_path, "r") as file:
+    imported_json = json.load(file)
 
-    # /ΑΝΤΩΝΗΣ ΚΑΤΙΝΑΡΗΣ/songs/ΤΙ ΝΑ ΣΟΥ ΚΑΝΕΙ ΜΙΑ ΚΑΡΔΙΑ
-    text = text.lower() 
-    # print(song_name)
-    for artist_value in imported_json.values():
-        for song_name, song_value in artist_value["songs"].items():
-            if text in song_name.lower() or text in song_value["song_en_name"].lower():
-                print(song_value["song_en_name"])
-                print(f"https://www.youtube.com/watch?v={song_value["demo_id"]}")
+with open(file_path, "w", encoding="utf-8") as file:
+    json.dump(imported_json, file, ensure_ascii=False, indent=4)
 
-def count_songs():
-    with open("crawling_scripts/youtube_songs.json", "r") as file:
-        imported_json = json.load(file)
 
-    all_songs =[]
-    for artist_value in imported_json.values():
-        artist_songs = artist_value["songs"].keys()
-        all_songs.extend(artist_songs)
-    return len(all_songs)
+
+
+
+
 
